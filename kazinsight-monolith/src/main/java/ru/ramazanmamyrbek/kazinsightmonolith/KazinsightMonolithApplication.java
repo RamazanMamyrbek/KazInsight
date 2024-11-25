70f0f825af134070157c1e44a1bc22988a72f51b
RamazanMamyrbek <rama.mamirbek@gmail.com>
2024-11-25 12:42:28 +0500

    First init

---
+++ kazinsight-monolith/src/main/java/ru/ramazanmamyrbek/kazinsightmonolith/KazinsightMonolithApplication.java
@@ -0,0 +1,13 @@
+package ru.ramazanmamyrbek.kazinsightmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KazinsightMonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KazinsightMonolithApplication.class, args);
+    }
+
+}
---
+++ kazinsight-monolith/src/test/java/ru/ramazanmamyrbek/kazinsightmonolith/KazinsightMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ramazanmamyrbek.kazinsightmonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KazinsightMonolithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
